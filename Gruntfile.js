da12d5f446aae6449563aaca50e07b683b689370
Navjot Tomer <navjottomer@gmail.com>
2025-02-24 16:06:18 +0530

    Replace node-sass with sass in Gruntfile and update package.json dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -224,7 +224,7 @@ module.exports = function (grunt) {
         },
         sass: {
             options: {
-                implementation: require('node-sass'),
+                implementation: require('sass'),
                 style: 'nested'
             },
             dist: {
