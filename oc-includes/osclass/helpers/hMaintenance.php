3b476b37cfc5f421f2234e6c579ca556d2d3792b
navjottomer <navjottomer@gmail.com>
2026-09-14 06:15:04 +0000

    maintenance: use the Mindstellar-only header on the new helper file

--- oc-includes/osclass/helpers/hMaintenance.php
+++ oc-includes/osclass/helpers/hMaintenance.php
@@ -1,12 +1,9 @@
 <?php
 /*
  * This file is part of Shopclass (Mindstellar).
- * Copyright (c) 2014 Osclass (original work, licensed under the Apache License 2.0)
  * Copyright (c) 2021-2026 Mindstellar Community
  *
- * Distributed under the GNU General Public License v3.0 or later. The original
- * Osclass code it derives from was licensed under the Apache License 2.0.
- * See LICENSE (GPL-3.0) and LICENSE-APACHE (Apache-2.0).
+ * Distributed under the GNU General Public License v3.0 or later. See LICENSE.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
